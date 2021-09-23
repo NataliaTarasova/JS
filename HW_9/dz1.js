3f46a73fa86c80208dda20f9ebc3dfb4bc63f89c
NataliaTarasova <lukynate@yandex.ru>
2021-09-23 21:50:01 +0300

    Домашнее задание 9(ООП, классы)

---
+++ HW_9/dz1.js
@@ -0,0 +1,12 @@
+class Marker{
+    constructor(color,count){
+        this.color=color;
+        this.count=count;
+    }
+    exchangeColor(){
+    console.log(this.color);
+    }
+}
+
+let print =exchangeColor("Привет!");
+print(red)
---
+++ HW_9/dz3.js
@@ -0,0 +1,13 @@
+class Employee{
+    constructor(surname,name,age,profession){
+    this.surname=surname;
+    this.name=name;
+    this.age=age;
+    this.profession=profession;
+    this.arrWorker=[];
+    }
+}
+
+class EmpTable{
+    
+}
