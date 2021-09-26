47eaad89858d43cbf60fc15bf320b379e311b825
NataliaTarasova <lukynate@yandex.ru>
2021-09-26 11:24:38 +0300

    удалить?

---
+++ HW_6/index.js
@@ -0,0 +1,55 @@
+
+
+let shoppingList=[
+    {   
+        id:1,
+        nameProduct:'milk',
+        count:1,
+        bought:'yes',
+    },
+    
+   {
+        id:2,
+        nameProduct:'bread',
+        count:1,
+        bought:'no',
+    },
+    
+   {
+        id:3,
+        nameProduct:'chicken',
+        count:2,
+        bought:'yes',
+    },
+    
+   {
+       id:4,
+        nameProduct:'fish',
+        count:1,
+        bought:'no',
+    }
+
+
+];
+
+let listYes =shoppingList.filter(shoppingList=>shoppingList.bought=='yes');
+let listNo =shoppingList.filter(shoppingList=>shoppingList.bought=='no');
+console.log (listNo);
+console.log (listYes);
+
+
+/*
+let listPush= function listPushNew(){
+    if (nameProduct=this.nameProduct){
+        return count+this.count
+    };
+}
+
+
+let listPushNew = function(){
+    if (shoppingList.bought=='yes')
+    return shoppingList.nameProduct;
+}
+
+listPushNew();
+console.log(listPushNew)*/
