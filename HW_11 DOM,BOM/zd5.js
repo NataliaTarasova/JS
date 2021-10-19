f1e92e8af944bee1ea204107c26422cec3524392
NataliaTarasova <lukynate@yandex.ru>
2021-10-19 20:08:17 +0300

    ДЗ_11 дополнила пункты 3,5,8

--- HW_11 DOM,BOM/zd5.js
+++ HW_11 DOM,BOM/zd5.js
@@ -1,50 +1,50 @@
-let button =document.getElementById('button')
+let generate =document.getElementById('button')
+let months=document.getElementById('month')
+let years=document.getElementById('year')
 
-//button.onclick=
+
+generate.addEventListener("click",    /// не смогла связать с кнопкой
+ function (){
+   createCalendar();
+  });
 
 function createCalendar(elem, year, month) {
 
-    let mon = month - 1; // месяцы в JS идут от 0 до 11, а не от 1 до 12
-    let d = new Date(year, mon);
+  let mon = month - 1; 
+  let d = new Date(year, mon);
 
-    let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
+  let table = '<table><tr><th>пн</th><th>вт</th><th>ср</th><th>чт</th><th>пт</th><th>сб</th><th>вс</th></tr><tr>';
 
-    // пробелы для первого ряда
-    // с понедельника до первого дня месяца
-    // * * * 1  2  3  4
-    for (let i = 0; i < getDay(d); i++) {
+  for (let i = 0; i < getDay(d); i++) {
+    table += '<td></td>';
+  }
+
+  while (d.getMonth() == mon) {
+    table += '<td>' + d.getDate() + '</td>';
+
+    if (getDay(d) % 7 == 6) { 
+      table += '</tr><tr>';
+    }
+
+    d.setDate(d.getDate() + 1);
+  }
+
+  if (getDay(d) != 0) {
+    for (let i = getDay(d); i < 7; i++) {
       table += '<td></td>';
     }
-
-    // <td> ячейки календаря с датами
-    while (d.getMonth() == mon) {
-      table += '<td>' + d.getDate() + '</td>';
-
-      if (getDay(d) % 7 == 6) { // вс, последний день - перевод строки
-        table += '</tr><tr>';
-      }
-
-      d.setDate(d.getDate() + 1);
-    }
-
-    // добить таблицу пустыми ячейками, если нужно
-    // 29 30 31 * * * *
-    if (getDay(d) != 0) {
-      for (let i = getDay(d); i < 7; i++) {
-        table += '<td></td>';
-      }
-    }
-
-    // закрыть таблицу
-    table += '</tr></table>';
-
-    elem.innerHTML = table;
   }
 
-  function getDay(date) { // получить номер дня недели, от 0 (пн) до 6 (вс)
-    let day = date.getDay();
-    if (day == 0) day = 7; // сделать воскресенье (0) последним днем
-    return day - 1;
-  }
+  table += '</tr></table>';
 
-  createCalendar(calendar, 2021, 10);
+  elem.innerHTML = table;
+
+
+function getDay(date) { 
+  let day = date.getDay();
+  if (day == 0) day = 7; 
+  return day - 1;
+}
+}
+
+createCalendar(calendar,2021,10)  //вызов функции
